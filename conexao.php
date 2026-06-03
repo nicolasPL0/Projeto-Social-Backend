408b680a2ddaec66702c6c96a127af79a6accc4c
nicolasPL0 <calangosad2012@gmail.com>
2026-06-03 10:48:33 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -4,9 +4,6 @@ $host     = getenv('MYSQLHOST')     ?: 'localhost';
 $port     = getenv('MYSQLPORT')     ?: '3306';
 $username = getenv('MYSQLUSER')     ?: 'root';
 $password = getenv('MYSQLPASSWORD') ?: '';
-
-// TESTE CRUCIAL: O Railway quase sempre usa 'railway' como nome do banco
-// mesmo que você tenha criado as tabelas manualmente.
 $dbname   = getenv('MYSQLDATABASE') ?: 'railway'; 
 
 try {
@@ -14,7 +11,6 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } catch (PDOException $e) {
-    // Se der erro, vamos tentar o banco 'projeto_social' como plano B
     try {
         $dbname = 'projeto_social';
         $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
