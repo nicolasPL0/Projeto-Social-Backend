d0063fdd3682ca113a442dce3a394a80daaca0c5
nicolasPL0 <calangosad2012@gmail.com>
2026-06-08 11:24:26 -0300

    Update teste.php

--- teste.php
+++ teste.php
@@ -18,4 +18,20 @@ try {
     echo "<h2>❌ Erro ao ler a tabela 'alunos':</h2>";
     echo "<p>Mensagem do banco: " . $e->getMessage() . "</p>";
 }
+    try {
+    $stmt = $pdo->query("SELECT * FROM registro LIMIT 5");
+    $resultados2 = $stmt->fetchAll();
+    
+    echo "<h2>✅ O PHP acessou a tabela 'alunos' com sucesso!</h2>";
+    if (empty($resultados)) {
+        echo "<p>A tabela está funcionando, mas está <strong>vazia</strong>. Nenhum aluno cadastrado ainda.</p>";
+    } else {
+        echo "<p>Alunos encontrados:</p><pre>";
+        print_r($resultados);
+        echo "</pre>";
+    }
+} catch (PDOException $e) {
+    echo "<h2>❌ Erro ao ler a tabela 'alunos':</h2>";
+    echo "<p>Mensagem do banco: " . $e->getMessage() . "</p>";
+}
 ?>
